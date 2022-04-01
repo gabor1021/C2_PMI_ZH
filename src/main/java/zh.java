3740b0e3fb00ee29bc769caa4d34bbc54bf79dde
Win10 <pgabor1021@gmail.com>
2022-04-01 10:57:48 +0200

    1

--- src/main/java/zh.java
+++ src/main/java/zh.java
@@ -5,20 +5,32 @@ import java.util.Scanner;
 public class zh {
     public static void main(String[] args) {
         //elso();
-        masodik();
+        //masodik();
+        harmadik();
     }
-    public static void elso(){
+
+    public static void elso() {
         Matrix elso = new Matrix();
         elso.feltolt(); //3x3 mátrix feltöltése billentyűzetről (soronként haladva)
         System.out.println("Nem 0 értékeket száma: " + elso.numberOfNonZeroValues());
         elso.indexOfColumnsWithSameValues();
     }
-    public static void masodik(){
-        Team elso=new Team();
+
+    public static void masodik() {
+        Team elso = new Team();
         elso.newMember();
         elso.newMember();
         System.out.println(elso.getMembers());
         elso.leaveTeam();
+        System.out.println(elso.getMembers());
+    }
+
+    public static void harmadik() {
+        PositionChanges elso = new PositionChanges();
+        elso.newMember();
+        elso.newMember();
+        System.out.println(elso.getMembers());
+        elso.promote();
         System.out.println(elso.getMembers());
     }
 }
@@ -59,40 +71,49 @@ class Matrix {
         }
     }
 }
-class Team{
-    Scanner in= new Scanner(System.in);
-    protected ArrayList<String> name=new ArrayList<>();
-    protected ArrayList<String> position=new ArrayList<>();
-    public void newMember(){
+
+class Team {
+    Scanner in = new Scanner(System.in);
+    protected ArrayList<String> name = new ArrayList<>();
+    protected ArrayList<String> position = new ArrayList<>();
+
+    public void newMember() {
         System.out.println("Új csapattag hozzáadása");
         System.out.println("Név:");
         name.add(in.nextLine());
         System.out.println("Pozíció:");
         position.add(in.nextLine());
     }
-    public void leaveTeam(){
+
+    public void leaveTeam() {
         System.out.println("Hányadik csapattagot töröljem?");
-        int index=in.nextInt();
-        name.remove(index-1);
-        position.remove(index-1);
+        int index = in.nextInt();
+        try {
+            name.remove(index - 1);
+            position.remove(index - 1);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Nincs ilyen csapattag");
+        }
     }
-    public String getMembers(){
-        String members="";
-        for(int i=0;i< name.size();i++){
-            members=members+name.get(i)+", "+position.get(i)+"\n";
+
+    public String getMembers() {
+        String members = "";
+        for (int i = 0; i < name.size(); i++) {
+            members = members + name.get(i) + ", " + position.get(i) + "\n";
         }
         return members;
     }
 }
-class PositionChanges extends Team{
-    public void promote(){
+
+class PositionChanges extends Team {
+    public void promote() {
         int index;
         String pos;
-        Scanner in= new Scanner(System.in);
-        System.out.println("Hányadik csapattagot kell előléptetni?");
-        index=in.nextInt();
+        Scanner in = new Scanner(System.in);
         System.out.println("Új pozíció:");
-        pos=in.nextLine();
-        position.set(index-1,pos);
+        pos = in.nextLine();
+        System.out.println("Hányadik csapattagot kell előléptetni?");
+        index = in.nextInt();
+        position.set(index-1, pos);
     }
 }
